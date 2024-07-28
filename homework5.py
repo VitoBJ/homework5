945a0e434e9b66248c1c6a3e8357fdf88acc1739
VitoBJ <Shizane7@gmail.com>
2024-07-28 23:11:12 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,17 @@
+
+immutable_var1= 1,2,3, False
+immutable_var2= (1,2,3)
+immutable_var3= ([1,2,3])
+print(immutable_var1)
+print(immutable_var2)
+print(immutable_var3)
+mutable_list=([123,16],7)
+print(mutable_list)
+mutable_list[0] [0]=120
+print(mutable_list)
+
+
+
+
+
+
